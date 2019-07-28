ca99ce91eca048e854b67d5810267a8e809995f9
vismi <vismi.25@gmail.com>
2019-07-28 13:57:00 +0530

    flight selection functionality

--- multi-city-flight-search/src/app/flight-list/flight-list.component.ts
+++ multi-city-flight-search/src/app/flight-list/flight-list.component.ts
@@ -33,7 +33,7 @@ export class FlightListComponent implements OnInit {
        href = this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex].nextFlightSegment.link.href;
      }
 
-     if(href!=''){
+     if(href!='' && this.journeyData.length=this.ondSearchPayload.ondsearches.length){
     this.searchBarService.getSearchDataForNextSegment(href,this.ondSearchPayload)
     .subscribe((response)=>{
       if(response!='No Offers'){
@@ -49,8 +49,8 @@ export class FlightListComponent implements OnInit {
         this.journeyData.push({segmentData : response});
       }
      });
-   }else if(this.journeyData.length=this.ondSearchPayload.ondsearches.length){
-     console.log('selection complete');
+   }else {
+     this.router.navigate(['/summary']);
    }
   }
 
