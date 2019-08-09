e52846e609886837936d031303fc11d3d7c9cac5
rajputkhushboo <rajputgorkhu17@gmail.com>
2019-08-09 19:07:34 +0530

    responsive css changes

--- multi-city-flight-search/src/app/flight-list/flight-list.component.ts
+++ multi-city-flight-search/src/app/flight-list/flight-list.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { SearchBarService } from '../../app/search-bar/search-bar.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {SearchBarService} from '../../app/search-bar/search-bar.service';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-flight-list',
   templateUrl: './flight-list.component.html',
   styleUrls: ['./flight-list.component.scss'],
-   providers:[SearchBarService]
+  providers: [SearchBarService]
 })
 export class FlightListComponent implements OnInit {
 
@@ -18,83 +18,84 @@ export class FlightListComponent implements OnInit {
 
   selectedJourneyItems: any;
 
-  constructor(private searchBarService:SearchBarService, private router:Router) { }
+  constructor(private searchBarService: SearchBarService, private router: Router) {
+  }
 
   ngOnInit() {
-    this.selectedJourneyItems= [];
+    this.selectedJourneyItems = [];
 
   }
 
-  flightsSelected: Array<any> = [];
+  flightsSelected: any = [];
 
 
+  callNextSegment(segmentIndex: any, flightIndex: any, cabinIndex: any) {
+    var href = '';
+    var selectionObject = {
+      'cabin': {},
+      'localFlightSegments': {},
 
-  callNextSegment(segmentIndex:any, flightIndex:any, cabinIndex:any){
-     var href = '';
-     var selectionObject = {
-       'cabin':{},
-       'localFlightSegments':{},
+    };
 
-     };
-
-     selectionObject.cabin = this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex];
-     selectionObject.localFlightSegments = this.journeyData[segmentIndex].segmentData[flightIndex].localFlightSegments[0];
+    selectionObject.cabin = this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex];
+    selectionObject.localFlightSegments = this.journeyData[segmentIndex].segmentData[flightIndex].localFlightSegments[0];
 
 
-     if(this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex].hasOwnProperty('nextFlightSegment')){
-       href = this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex].nextFlightSegment.link.href;
-     }
+    if (this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex].hasOwnProperty('nextFlightSegment')) {
+      href = this.journeyData[segmentIndex].segmentData[flightIndex].availableCabinsForOption[cabinIndex].nextFlightSegment.link.href;
+    }
 
-     if(href!='' ){
-    this.searchBarService.getSearchDataForNextSegment(href,this.ondSearchPayload)
-    .subscribe((response)=>{
+    if (href != '') {
+      this.searchBarService.getSearchDataForNextSegment(href, this.ondSearchPayload)
+        .subscribe((response) => {
 
-        if(segmentIndex+1<this.journeyData.length){
-          console.log('prev option changed');
-          this.segmentFlightSelectedFor=segmentIndex;
-          let timesToPop = this.journeyData.length;
-          this.journeyData.splice(segmentIndex+1);
-          this.selectedJourneyItems.splice(segmentIndex);
-        }else{
-          console.log('normal');
-          this.segmentFlightSelectedFor++;
-        }
-        this.journeyData.push({segmentData : response});
-        this.selectedJourneyItems.push(selectionObject);
-        let flightListElement = document.getElementById('segment'+segmentIndex);
-        console.log('flightListElement',flightListElement);
-        flightListElement.scrollIntoView({'behavior':'smooth'});
-      
-     });
-   }else if(this.journeyData.length==this.ondSearchPayload.ondSearches.length) {
-     this.selectedJourneyItems.push(selectionObject);
-     this.router.navigate(['/summary'],{state: {data: this.selectedJourneyItems}});
+          if (segmentIndex + 1 < this.journeyData.length) {
+            console.log('prev option changed');
+            this.segmentFlightSelectedFor = segmentIndex;
+            let timesToPop = this.journeyData.length;
+            this.journeyData.splice(segmentIndex + 1);
+            this.selectedJourneyItems.splice(segmentIndex);
+          } else {
+            console.log('normal');
+            this.segmentFlightSelectedFor++;
+          }
+          this.journeyData.push({segmentData: response});
+          this.selectedJourneyItems.push(selectionObject);
+          let flightListElement = document.getElementById('segment' + segmentIndex);
+          console.log('flightListElement', flightListElement);
+          flightListElement.scrollIntoView({'behavior': 'smooth'});
 
-   }
+        });
+    } else if (this.journeyData.length == this.ondSearchPayload.ondSearches.length) {
+      this.selectedJourneyItems.push(selectionObject);
+      this.router.navigate(['/summary'], {state: {data: this.selectedJourneyItems}});
+
+    }
   }
 
-  sort(segmentIndex:any, sortParams:any){
-    var segmentData= this.journeyData[segmentIndex].segmentData;
-    switch(sortParams) {
-   case "duration": {
-     segmentData.sort(function(a, b){
-       var x = a.localFlightSegments[0].duration.toLowerCase(), y = b.localFlightSegments[0].duration.toLowerCase();
-       return x < y ? -1 : x > y ? 1 : 0;
-     });
-      break;
-   }
-   case "departure": {
-     segmentData.sort(function(a, b){
-       var x = a.localFlightSegments[0].departureDateTime.toLowerCase(), y = b.localFlightSegments[0].departureDateTime.toLowerCase();
-       return x < y ? -1 : x > y ? 1 : 0;
-     });
-      break;
-   }
-   default: {
-      console.log("Invalid choice");
-      break;
-   }
-}
+  sort(segmentIndex: any, sortParams: any) {
+    var segmentData = this.journeyData[segmentIndex].segmentData;
+    switch (sortParams) {
+      case "duration": {
+        segmentData.sort(function (a, b) {
+          var x = a.localFlightSegments[0].duration.toLowerCase(), y = b.localFlightSegments[0].duration.toLowerCase();
+          return x < y ? -1 : x > y ? 1 : 0;
+        });
+        break;
+      }
+      case "departure": {
+        segmentData.sort(function (a, b) {
+          var x = a.localFlightSegments[0].departureDateTime.toLowerCase(),
+            y = b.localFlightSegments[0].departureDateTime.toLowerCase();
+          return x < y ? -1 : x > y ? 1 : 0;
+        });
+        break;
+      }
+      default: {
+        console.log("Invalid choice");
+        break;
+      }
+    }
 
   }
 }
--- multi-city-flight-search/src/app/search-bar/search-bar.component.ts
+++ multi-city-flight-search/src/app/search-bar/search-bar.component.ts
@@ -1,108 +1,113 @@
-import { Component, OnInit } from '@angular/core';
-import { SearchBarService } from '../search-bar/search-bar.service';
+import {Component, OnInit} from '@angular/core';
+import {SearchBarService} from '../search-bar/search-bar.service';
 import * as moment from 'moment';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import Swal from 'sweetalert2';
+import {Observable} from 'rxjs/Rx';
 
-const Swal = require('sweetalert2');
+
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
-   providers:[SearchBarService]
+  providers: [SearchBarService]
 })
 export class SearchBarComponent implements OnInit {
 
-totalSegments = 1;
-segmentFlightSelectedFor = 0;
-ondSearchPayload: any = { 'ondSearches': []};
-journeyMatrix: Array<any> = [];
-showFlightList = false;
-response : any={};
-flightList : any =[];
-ondSearches : any;
-journeyData : Array<any> =[];
- userData: FormGroup;
-    submitted = false;
+  totalSegments = 1;
+  segmentFlightSelectedFor = 0;
+  ondSearchPayload: any = {'ondSearches': []};
+  journeyMatrix: any = [];
+  showFlightList = false;
+  response: any = {};
+  flightList: any = [];
+  ondSearches: any;
+  showLoadingImg: boolean = false;
+  journeyData: any = [];
+  userData: FormGroup;
+  ticks: any;
+  submitted = false;
 
- constructor(private searchBarService:SearchBarService , private formBuilder:FormBuilder) { }
+  constructor(private searchBarService: SearchBarService, private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
 
-  this.userData = this.formBuilder.group({
-            originLocationCode: ['',[Validators.required , Validators.minLength(3)]],
-            destinationLocationCode: ['', [Validators.required , Validators.minLength(3)]],
-            departureDate: ['', Validators.required]
-        });
+    this.userData = this.formBuilder.group({
+      originLocationCode: ['', [Validators.required, Validators.minLength(3)]],
+      destinationLocationCode: ['', [Validators.required, Validators.minLength(3)]],
+      departureDate: ['', Validators.required]
+    });
 
     this.journeyMatrix.push({
-    'originLocationCode':'',
-    'destinationLocationCode': '',
-    'departureDate':moment().add(0, 'days').format('YYYY-MM-DD')
-  });
+      'originLocationCode': '',
+      'destinationLocationCode': '',
+      'departureDate': moment().add(0, 'days').format('YYYY-MM-DD')
+    });
   }
 
 
-    onSubmit() {
-        this.submitted = true;
+  onSubmit() {
+    this.submitted = true;
 
-        if (this.userData.invalid) {
-            return;
-        }
-        }
-
-
-addSegment(){
-  var lastDestination = this.journeyMatrix[this.journeyMatrix.length-1].destinationLocationCode;
-  var lastDate = this.journeyMatrix[this.journeyMatrix.length-1].departureDate;
-
-if(this.totalSegments<6){  this.journeyMatrix.push({
-    'originLocationCode': lastDestination,
-    'destinationLocationCode': '',
-    'departureDate':moment(lastDate).add(7, 'days').format('YYYY-MM-DD')
-  });
-  this.totalSegments++;}
-}
-
-removeSegment(segmentIndex:any){
-  if(this.totalSegments>1 && segmentIndex>0){
-    this.journeyMatrix.splice(segmentIndex,1)
-    this.totalSegments--;
+    if (this.userData.invalid) {
+      return;
+    }
   }
-}
-
-getdata(data : any){
-  this.journeyData =[];
-  this.ondSearchPayload.ondSearches = this.journeyMatrix;
 
 
-  
-  this.searchBarService.deleteCache()
-.subscribe((resp)=>{
-  this.searchBarService.getSearchData(this.ondSearchPayload)
-.subscribe(
-	data =>{
-		var response=data;
+  addSegment() {
+    var lastDestination = this.journeyMatrix[this.journeyMatrix.length - 1].destinationLocationCode;
+    var lastDate = this.journeyMatrix[this.journeyMatrix.length - 1].departureDate;
 
-    this.showFlightList = true;
-    this.journeyData.push({segmentData : response});
-    document.getElementById('flight-list-outer').scrollIntoView();
-	Swal.fire({
-  title: 'Error!',
-  text: 'Do you want to continue',
-  type: 'error',
-  confirmButtonText: 'Cool'
-});
-  
- },
-	error=>{
-  console.log('error',error);
- }
-	);
- });
- 
- 
- 
- 
-}
+    if (this.totalSegments < 6) {
+      this.journeyMatrix.push({
+        'originLocationCode': lastDestination,
+        'destinationLocationCode': '',
+        'departureDate': moment(lastDate).add(7, 'days').format('YYYY-MM-DD')
+      });
+      this.totalSegments++;
+    }
+  }
+
+  removeSegment(segmentIndex: any) {
+    if (this.totalSegments > 1 && segmentIndex > 0) {
+      this.journeyMatrix.splice(segmentIndex, 1)
+      this.totalSegments--;
+    }
+  }
+
+  getdata(data: any) {
+    this.showLoadingImg = true;
+    this.journeyData = [];
+    this.ondSearchPayload.ondSearches = this.journeyMatrix;
+
+
+    let timer = Observable.timer(200000, 1000);
+    this.searchBarService.deleteCache()
+      .subscribe((resp) => {
+        this.searchBarService.getSearchData(this.ondSearchPayload)
+          .subscribe(
+            data => {
+              var response = data;
+              this.showLoadingImg = false;
+              this.showFlightList = true;
+              this.journeyData.push({segmentData: response});
+              document.getElementById('flight-list-outer').scrollIntoView();
+
+              timer.subscribe(t => this.ticks = t);
+
+            }, error => {
+              this.showLoadingImg = false;
+              console.log('error', error.error.errors[0].code);
+              Swal.fire({
+                title: error.error.errors[0].code,
+                text: error.error.errors[0].reason == 'UNEXPECTED_TECHNICAL_EXCEPTION' ? 'Route not serviced. Search again' : 'not UNEXPECTED_TECHNICAL_EXCEPTION',
+                type: 'error',
+                confirmButtonText: 'Cool'
+              })
+            }
+          );
+      });
+  }
 }
